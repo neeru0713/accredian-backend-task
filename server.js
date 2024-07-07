89e6079c325c31d1baabc59ac29ed90fa24d7801
neeru0713 <neerurani1307@gmail.com>
2024-07-07 17:59:38 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Backend server running on PORT", port);
+});
